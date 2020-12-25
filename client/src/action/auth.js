7a950f20b34418c331fe199e47ec6759d3ea7c75
hamouda mdemagh <66480000+HRL-cody@users.noreply.github.com>
2020-12-25 03:03:45 +0100

    fourth commit

--- client/src/action/auth.js
+++ client/src/action/auth.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import {setAlert} from './alert';
 import { REGISTER_SUCCESS, REGISTER_FAIL,USER_LOADED,
   AUTH_ERROR, LOGIN_SUCCESS,
-  LOGIN_FAIL , LOGOUT } from "./types";
+  LOGIN_FAIL , LOGOUT, CLEAR_PROFILE } from "./types";
 import setAuthToken from '../helper/setAuthToken'
 //LoadUser
 export const loadUser = () => async dispatch => {
@@ -89,5 +89,6 @@ export const login = ( email, password ) => async (dispatch) => {
  // Logout / clear Profile
 
  export const logout = () => dispatch => {
+   dispatch({type: CLEAR_PROFILE})
    dispatch({ type: LOGOUT});
  }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/heading-has-content */
 import React, { useEffect } from "react";
+import {Link} from "react-router-dom"
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../action/profile";
@@ -9,7 +10,7 @@ import { Fragment } from "react";
 //Try to capitalize 'app' like
 const Dashboard = ({
   getCurrentProfile,
-  auth,
+  auth:{user},
   profile: { profile, loading },
 }) => {
   useEffect(() => {
@@ -17,16 +18,11 @@ const Dashboard = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return loading && profile === null ? (
-    <Loader />
-  ) : (
-    <Fragment>
-      <h1 className="large text-primary"></h1>
-      <p className="lead">
-        {" "}
-        <i className="fas-fa-user">Welcome{user && user.name}</i>{" "}
-      </p>
-    </Fragment>
+  return loading && profile === null ? (<Loader />) : (<Fragment><h1 className="large text-primary"></h1><p className="lead">{" "}<i className="fas fa-user">Welcome {user && user.name}</i>{" "}</p>
+  {profile !== null ? (<Fragment>has</Fragment>) : (<Fragment> <p>You hav no profile, please add some info</p>
+  <Link to='/creat-profile' className='btn btn-primary my-1'>Creat Profile</Link>
+   </Fragment>)}
+  </Fragment>
   );
 };
 
